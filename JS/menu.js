0e3f51ef64e1dea21d037c9d1f24c380fe48a337
Adrián <adriancoroneltandil@gmail.com>
2023-11-15 17:34:41 -0300

    Creado menu desplegable y enlaces, creacion de nuevos html prontos a realizar

---
+++ JS/menu.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var menuDropdown = document.getElementById('menuDropdown');
+    menuDropdown.classList.toggle('show');
+});
